1a275a2d1001c3f35cdd36a8a5ae6a98b6580768
gisella agnesia <gisellaagnesia@gmail.com>
2021-02-26 09:35:30 +0700

    aktifitas 2

---
+++ aktifitas2.py
@@ -0,0 +1,16 @@
+#operator
+
+bil1=int(input("Masukkan Angka Pertama: "))
+bil2=int(input("Masukkan Angka Kedua: "))
+
+jumlah=bil1+bil2
+kurang=bil1-bil2
+kali=bil1*bil2
+bagi=bil1/bil2
+modulus=bil1%bil2
+
+print("Hasil dari %d + %d = %d" % (bil1, bil2, jumlah))
+print("Hasil dari %d - %d = %d" % (bil1, bil2, kurang))
+print("Hasil dari %d * %d = %d" % (bil1, bil2, kali))
+print("Hasil dari %d / %d = %d" % (bil1, bil2, bagi))
+print("Hasil dari %d mod %d = %d" % (bil1, bil2, modulus))
