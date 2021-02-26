2da1f355d10bf72dfce493ab5a31ad4e9deb9927
gisella agnesia <gisellaagnesia@gmail.com>
2021-02-26 09:47:17 +0700

    aktifitas 3

---
+++ aktifitas3.py
@@ -0,0 +1,14 @@
+#percabangan
+
+print("Percabangan Pada Bahasa Pemrograman Python")
+print("Pilih Bahasa Pemrograman: "
+print("1. Phyton")
+print("2. C")
+
+pilih=int(input(Pilihan Menu (1/2): "))
+if pilih==1;
+	print("Bahasa Pemrograman yang dipilih Adalah Python") 
+if pilih==2;
+	print("Bahasa Pemrograman yang dipilih Adalah C")
+else:
+	print("Maaf Pilihan yang Anda Pilih Tidak Tersedia")  
