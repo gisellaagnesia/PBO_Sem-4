4b83e8a21381a4537d99af36c073d5a41ec4b1b9
gisella agnesia <gisellaagnesia@gmail.com>
2021-02-26 10:05:39 +0700

    aktifitas 4

---
+++ aktifitas4.py
@@ -0,0 +1,10 @@
+print("Contoh Pengulangan Suatu String")
+for huruf in "SISTEL":
+	print(huruf)
+
+nilai_awal=0
+nilai_akhir=25 #tujuan/berapa kali
+step=5 #jeda antar loop
+print("Contoh Perulangan Angka Dengan Range ()")
+for i in range(nilai_awal, nilai_akhir, step):
+	print(i)
