188a622ab2ca6dafbb0445cbc896ff6d19b6c0fc
gisella agnesia <gisellaagnesia@gmail.com>
2021-02-26 09:27:22 +0700

    aktifitas 1

---
+++ aktifitas1.py
@@ -0,0 +1,12 @@
+#input 
+
+nama=input("Masukkan Nama Anda: Gisella Agnesia")
+umur=20
+
+#output
+
+print("")
+print("====Input dan Output Pada Bahasa Pemrograman====")
+print("Nama Anda: Gisella Agnesia")
+print("Umur Anda: 20 Tahun")
+print("")
